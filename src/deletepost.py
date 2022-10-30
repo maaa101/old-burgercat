5d91212d80ecdb1532ff205d32aedcff7fc7357d
maaa <85688939+maaa101@users.noreply.github.com>
2022-10-30 15:56:38 +0100

    Added post delete script
    Added post delete script, open it and type the post ID

---
+++ src/deletepost.py
@@ -0,0 +1,28 @@
+# Delete things from the burger database
+# used for moderation
+
+import sqlite3
+ 
+def get_db_connection():
+    conn = sqlite3.connect("database.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        print("Could not find post " + post_id)
+    return post
+
+def delete(id):
+    post = get_post(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+idinput = input("Which post do you want to delete?")
+
+delete(idinput + "")

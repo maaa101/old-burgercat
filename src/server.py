7811dcf73676c0c0f0e97c0a1feb063fc56f8319
maaa <85688939+maaa101@users.noreply.github.com>
2022-10-30 11:24:37 +0100

    made some progress burger

--- src/server.py
+++ src/server.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    render_template("/index/home.html")
+@app.route('/')
+def home():
+    return render_template("navbar.html") + render_template("home.html")
+
+@app.route('/post')
+def post():
+    return render_template("navbar.html") + render_template("post.html")
 
 if __name__ == "__main__":
     from waitress import serve

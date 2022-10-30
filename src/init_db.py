c7a2b51598cad8ca70a8b2a873ee8761dda65bc4
maaa <85688939+maaa101@users.noreply.github.com>
2022-10-30 12:53:26 +0100

    fixed up some stuff

---
+++ src/init_db.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+connection = sqlite3.connect("database.db")
+
+with open("schema.sql") as f:
+    connection.executescript(f.read())
+
+cur = connection.cursor()
+
+# add posts
+
+#cur.execute("INSERT INTO posts (title, imgurl) VALUES (?, ?)",
+#            ('the burger the burger is ready', 'https://tenor.com/view/yummy-burger-monch-cat-eat-gif-22806539')
+#            )
+
+#cur.execute("INSERT INTO posts (title, imgurl) VALUES (?, ?)",
+#            ('the burger revolution is here', 'https://media.discordapp.net/attachments/563966858462625822/1035545221536170096/ABA24D6F-E6E7-4718-B7CE-8B5A208AB1FE.gif')
+#            )
+
+connection.commit()
+connection.close()
--- src/server.py
+++ src/server.py
@@ -1,14 +1,39 @@
+import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect
+#from werkzeug.exceptions import abort
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "fart"
+
+def get_db_connection():
+    conn = sqlite3.connect("database.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+@app.route('/post', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form["title"]
+        imgurl = request.form["imgurl"]
+
+        if not title:
+            flash("do you think you can get away with posting a burger without a title")
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, imgurl) VALUES (?, ?)',
+                         (title, imgurl))
+            conn.commit()
+            conn.close()
+            return render_template("donepost.html")
+
+    return render_template("navbar.html") + render_template("post.html")
 
 @app.route('/')
 def home():
-    return render_template("navbar.html") + render_template("home.html")
-
-@app.route('/post')
-def post():
-    return render_template("navbar.html") + render_template("post.html")
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template("navbar.html") + render_template("home.html", posts=posts)
 
 if __name__ == "__main__":
     from waitress import serve
